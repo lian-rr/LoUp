9e1f97dfc579f4e30657962f2ff22418a3aa0e6e
Lian <lian_rr@hotmail.com>
2016-10-27 15:46:19 -0600

    Markers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -60,11 +60,24 @@ function ($scope, $stateParams) {
 
   google.maps.event.addDomListener(window, 'load', function() {
     var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+
     var mapOptions = {
       center: myLatlng,
       zoom: 16,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
       styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#fe9f06"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#eb5e01"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#eb5e01"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#46bcec"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#46bcec"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#46bcec"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#46bcec"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
+    };
+
+    var icons = {
+      current: {
+        icon: ''
+      },
+      restaurant: {
+        icon: '../img/restaurant.png'
+      },
+      bar: {
+        icon: '../img/bar.png'
+      }
     };
 
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
@@ -74,10 +87,33 @@ function ($scope, $stateParams) {
       var myLocation = new google.maps.Marker({
         position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
         map: map,
-        title: "My Location"
+        title: "My Location",
+        //icon: icons['current'].icon
       });
     });
 
+    function addMarker(feature) {
+      var marker = new google.maps.Marker({
+        position: feature.position,
+        icon: icons[feature.type].icon,
+        map: map
+      });
+    }
+
+    var features = [
+      {
+        position: new google.maps.LatLng(9.925089, -84.041818),
+        type: 'restaurant'
+      },
+      {
+        position: new google.maps.LatLng(9.923377, -84.042988),
+        type: 'bar'
+      }
+    ];
+
+    for (var i = 0, feature; feature = features[i]; i++) {
+      addMarker(feature);
+    }
     $scope.map = map;
   });
 });
